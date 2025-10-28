77744c66230daf7128691f522f19066b4c854294
nadhil7 <nadhunadhil33429@gmail.com>
2025-10-28 18:31:50 +0530

    sw

--- day8.js
+++ day8.js
@@ -7,4 +7,4 @@ var searchRange = function (nums, target) {
 };
 const target = 11;
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// console.log(searchRange(nums,target))
+console.log(searchRange(nums,target))
