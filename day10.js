aa8ec6278984dbf29839cb1e3a342a610bd93570
nadhil7 <nadhunadhil33429@gmail.com>
2025-11-01 17:53:55 +0530

    day11

---
+++ day10.js
@@ -0,0 +1,18 @@
+var reverseWords = function (s) {
+    // console.log(s)
+    let reverse = [];
+    for (let i = 0; i <= s.length; i++) {
+        if (s[0] == " ") {
+            continue;
+        }
+        else {
+            reverse.push(s[0])
+            continue;
+        }
+        if (s[i]){
+            
+        }
+    }
+};
+var s = "sky good at look"
+reverseWords(s)
