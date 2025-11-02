f186503fe172705a27b926faf4b28f0b2afd4c11
nadhil7 <nadhunadhil33429@gmail.com>
2025-11-02 17:52:50 +0530

    day12

---
+++ day12.js
@@ -0,0 +1,23 @@
+var findMaxConsecutiveOnes = function(nums) {
+    let count = 0;
+    let counters = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            if (count > 0) {
+                counters.push(count);
+                count = 0;
+            }
+            continue;
+        }
+        count++;
+        if (nums[i + 1] !== 1) {
+            counters.push(count);
+            count = 0;
+        }
+    }
+    if (counters.length === 0) return 0;
+    let largest = Math.max(...counters);
+    return largest;
+};
+const nums = [1, 2, 2, 3, 1, 1, 1];
+console.log(findMaxConsecutiveOnes(nums))
