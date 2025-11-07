ac858dd6b36ba17a9f6f62dad65cce3e6cbc3d4c
nadhil7 <nadhunadhil33429@gmail.com>
2025-11-07 20:14:21 +0530

    day14

---
+++ day15.js
@@ -0,0 +1,16 @@
+function nextGreatestLetter(letters, target) {
+    let left = 0;
+    let right = letters.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (letters[mid] <= target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return letters[left % letters.length];
+}
+const letters = ["c", "f", "j"];
+const target = "c"
+console.log(nextGreatestLetter(letters, target))
