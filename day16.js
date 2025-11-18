71065cee30965f06a2032c8183deb065a7c9e03a
nadhil7 <nadhunadhil33429@gmail.com>
2025-11-18 18:34:11 +0530

    done

--- day16.js
+++ day16.js
@@ -15,7 +15,7 @@
 //     return answer
 // };
 // const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 8, 12, 3, 4];
-// console.log(plusOne(digits))
+console.log(plusOne(digits))
 
 
 var plusOne = function (digits) {
